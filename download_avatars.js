c4707cd0d7f88b85a9fd9359487383eb68c7b0dd
verrixkio <verrixkio@gmail.com>
2019-02-11 23:56:12 +0000

    found avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,9 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Change getRepo function to parse the JSON string into an object
+// pass this object (an array of contributors) objects to the cb function
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var secrets = require('./secrets.js');
   var options = {
@@ -15,11 +18,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
     request(options, function(err, res, body) {
-      cb(err, body);
+      cb(err, JSON.parse(body));
   });
 }
 
 console.log(getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //Trying to access the object
+  var user =[]
+  
+  for (var i = 0; i < result.length; i++) {
+    user = result[i]
+    console.log(user.avatar_url)
+  }
 }));
