762507729f5cb3616c0dcb5c37a287b39cfbe4a5
verrixkio <verrixkio@gmail.com>
2019-02-11 22:28:01 +0000

    forgot to save fine then keep your secrets

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,8 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  var secrets = require('./secrets.js')
+  var secrets = require('./secrets.js');
   var options = {
   
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
